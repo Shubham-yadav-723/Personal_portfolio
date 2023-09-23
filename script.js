686a184e62544e749eb8d419aaf6ed52f95e80f9
Shubham Yadav <72902202+Shubham-yadav-723@users.noreply.github.com>
2023-09-23 20:57:53 +0530

    done education section

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".auto-type", {
+      strings: ["Software Developer", "Ethical Hacker"],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+    });
+  });
+  
